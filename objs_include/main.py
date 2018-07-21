05c9c1037718abaa52d1a7198bb07330f4e63e7b
鄭宇亨 <cyh@zhengyuhengs-Mac-mini.local>
2018-07-22 07:59:00 +0800

    data

--- objs_include/main.py
+++ objs_include/main.py
@@ -9,7 +9,6 @@ def calculation_dist(obj_xy_list,state_center):
         y_dist = objs_xy[1] - state_center[1]
         range = round(sqrt(pow(x_dist,2)+pow(y_dist,2)),3)
         if range < 1.5:
-            print(range)
             count += 1
     return count
 
@@ -27,8 +26,8 @@ if __name__ == '__main__':
     ## SINGLE ONE TEST
     path = '../projectData/{}_clr.csv'
     category = ['mrt', 'hsp_and_clc', 'illegal']
-
-    obj_df = pd.read_csv(path.format(category[0]))
+    num = 2
+    obj_df = pd.read_csv(path.format(category[num]))
     obj_df['km_x'] = obj_df.apply(lambda x: cordinate_to_km.lat_lon_to_km(x['lat'],x['lon'])['lat'], axis=1)
     obj_df['km_y'] = obj_df.apply(lambda x: cordinate_to_km.lat_lon_to_km(x['lat'],x['lon'])['lon'], axis=1)
     print(obj_df.info())
@@ -36,10 +35,10 @@ if __name__ == '__main__':
 
     for i in range(bigTable.count()['state_code']):
         state_center_km = bigTable.loc[i,'km_x'],bigTable.loc[i,'km_y']
-        print(state_center_km)
+        # print(state_center_km)
         # print(objs_x_list)
         # print(objs_y_list)
-        bigTable.loc[i,category[0]] = calculation_dist(objs_xy_list,state_center_km)
+        bigTable.loc[i,category[num]] = calculation_dist(objs_xy_list,state_center_km)
 
-    print(bigTable[bigTable[category[0]] > 1].sort_values(category[0]))
+    print(bigTable[bigTable[category[num]] > 1].sort_values(category[num],ascending=False))
     print(bigTable[bigTable['state_code'] == 553526927]['center'])
